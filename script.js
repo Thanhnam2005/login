267991e792564f499e8369669896f85870fc5497
Thanhnam2005 <103724345+Thanhnam2005@users.noreply.github.com>
2024-02-28 16:42:28 +0700

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
     // Kiểm tra thông tin đăng nhập
     if (username === 'Camly' && password === '01032005') {
         // Nếu thông tin đăng nhập chính xác, chuyển hướng người dùng đến trang chính
-        window.location.href = 'https://12985b4f-fff2-4842-8516-989813e5f348-00-1jsr4ktl3yuoz.sisko.replit.dev/'; // Thay 'trang-chinh.html' bằng đường dẫn đến trang chính của bạn
+        window.location.href = 'https://thanhnam2005.github.io/ILOVEYOU.CAMLY/';
     } else {
         // Nếu thông tin đăng nhập không chính xác, hiển thị thông báo lỗi
         alert('Sai thông tin, vui lòng nhập lại.');
